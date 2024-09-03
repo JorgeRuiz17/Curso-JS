1ca54a656488d6f4ac67157453ef5a2cfe6fcfa8
JorgeRuiz17 <jorgejesus.ruiz@alumnos.ulagos.cl>
2024-09-03 14:15:07 -0400

    Nuevos elementos

--- JS/Holamundo.js
+++ JS/Holamundo.js
@@ -1,2 +1,10 @@
 alert("Hola mundo!");
 console.log("Hola mundo");
+
+let numeros = [2, 4, 1, 3];
+
+numeros.forEach(function(numero){
+    console.log(numero);
+});
+
+console.log("Estos son Núnmeros");
---
+++ JS/Variables.js
@@ -0,0 +1,17 @@
+var estatura = 1.70;
+let peso = 69;
+let nombre = "Jorge";
+var edad;
+let ciudad;
+
+edad = 25;
+ciudad = "Castro";
+
+const apellido = "Ruiz";
+
+const nombrecompleto = nombre + " " + apellido; 
+console.log(nombrecompleto);
+
+let docente = "victor";
+let asignatura = ["Pro. Movil"];
+let age = "Nose"; 
