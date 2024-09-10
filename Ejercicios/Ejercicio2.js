2c621cd55df707304ef4a053ebd83357ce8e3a1e
JorgeRuiz17 <jorgejesus.ruiz@alumnos.ulagos.cl>
2024-09-10 18:56:53 -0300

    Ejercicio 2

---
+++ Ejercicios/Ejercicio2.js
@@ -0,0 +1,20 @@
+//Promedio
+
+let nota1 = parseFloat(prompt("Ingrese la primera nota (40%):")); //ingreso de notas por pantalla
+let nota2 = parseFloat(prompt("Ingrese la segunda nota (30%):"));
+let nota3 = parseFloat(prompt("Ingrese la tercera nota (30%):"));
+
+let promedio = (nota1 * 0.40) + (nota2 * 0.30) + (nota3 * 0.30); //se calcula el promedio según la el porcentaje
+
+console.log("El promedio es: " + promedio.toFixed(2));
+
+if (promedio < 3.95) {
+    console.log("El estudiante ha reprobado la asignatura con un promedio de: " + promedio.toFixed(2));
+}else if (promedio >= 3.95 && promedio <= 4.94) {
+    console.log("El estudiante va a examen con un primedio de: " + promedio.toFixed(2));
+}else if (promedio > 4.94){
+    console.log("El estudiante ha aprobado la asignatura con un promedio de: " + promedio.toFixed(2));
+}
+
+//toFixed se muestra el promedio con dos decimales
+//&& condicional y, los das condiciones deben ser verdaderas
