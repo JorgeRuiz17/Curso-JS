58a1ee588e7b6627924f53dc548472ff7f6b9dba
JorgeRuiz17 <jorgejesus.ruiz@alumnos.ulagos.cl>
2024-09-11 19:50:36 -0300

    reto

--- Ejercicios/Ejercicio3.js
+++ Ejercicios/Ejercicio3.js
@@ -1,1 +1,16 @@
-//ciclos, los 500 se van sumando 10 y los 400 se les va restando 2
+let suma1 = 500;
+let suma2 = 456;
+let total = 0;
+
+while (suma1 <= 800){
+    total += suma1 + suma2;
+    suma1 += 10;
+    suma2 -= 2;
+}
+
+alert("El resultado es: " + total);
+alert("suma 1 es: " + suma1);
+alert("suma 2 es: " + suma2);
+console.log("total: " + total)
+console.log("total suma: " + suma1)
+console.log("total resta: " + suma2)
---
+++ Retos/reto1.js
@@ -0,0 +1,6 @@
+let palabra = prompt("Ingrese una palabra: ");
+
+let contar = palabra.trim().split('a').length;
+contar -= 1; 
+alert("numero de a: " + contar);
+
