b8e37d7f1ea0e1b17bc1adf875195c58d93ce459
JorgeRuiz17 <jorgejesus.ruiz@alumnos.ulagos.cl>
2024-09-18 13:53:30 -0300

    Actualizacion 1

--- Retos/reto1.js
+++ Retos/reto1.js
@@ -1,6 +1,30 @@
-let palabra = prompt("Ingrese una palabra: ");
+let palabra = prompt("Ingrese una palabra: ").toLowerCase(); // permite cambiar string a minusculas
+let contadorA = 0;
+let contadorE = 0;
+let contadorI = 0;
+let contadorO = 0;
+let contadorU = 0;
 
-let contar = palabra.trim().split('a').length;
-contar -= 1; 
-alert("numero de a: " + contar);
 
+for (let i = 0; i < palabra.length; i++) { //se recorre el tamaño de la palabra(lenght), con el contardor i
+    let letra = palabra[i];
+    
+    if (letra === 'a') {
+        contadorA++;
+    } else if (letra === 'e') {
+        contadorE++;
+    } else if (letra === 'i') {
+        contadorI++;
+    } else if (letra === 'o') {
+        contadorO++;
+    } else if (letra === 'u') {
+        contadorU++;
+    }
+}
+
+
+alert("Número de 'a': " + contadorA);
+alert("Número de 'e': " + contadorE);
+alert("Número de 'i': " + contadorI);
+alert("Número de 'o': " + contadorO);
+alert("Número de 'u': " + contadorU);
